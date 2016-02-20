fb9da8f498f5da678263eda3e5492029126bd226
Brad Christensen <brad@christensen.co.nz>
2016-02-21 12:48:11 +1300

    Add Travis deployment config

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+mkdir build
+mv * build
+tar -czf mcpheeclansocietynz_com.tgz build
+export SSHPASS=$DEPLOY_PASS
+sshpass -e scp mcpheeclansocietynz_com.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/scripts/deployMcpheeClanSocietyNz.sh
