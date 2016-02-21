ec5530556d0c1a271a474e2f693d157d6ff572c8
Brad Christensen <brad@christensen.co.nz>
2016-02-21 13:13:19 +1300

    Update deploy path for build package

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ mkdir build
 mv * build
 tar -czf mcpheeclansocietynz_com.tgz build
 export SSHPASS=$DEPLOY_PASS
-sshpass -e scp mcpheeclansocietynz_com.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+sshpass -e scp mcpheeclansocietynz_com.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH/packages
 sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/scripts/deployMcpheeClanSocietyNz.sh
