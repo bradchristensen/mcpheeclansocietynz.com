a47f51d3b5776cfa813e0556dcf303d0bdd3e873
Brad Christensen <brad@christensen.co.nz>
2016-02-21 13:06:45 +1300

    Troubleshoot Travis config

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,6 @@ mkdir build
 mv * build
 tar -czf mcpheeclansocietynz_com.tgz build
 export SSHPASS=$DEPLOY_PASS
+dig $DEPLOY_HOST
 sshpass -e scp mcpheeclansocietynz_com.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
 sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/scripts/deployMcpheeClanSocietyNz.sh
